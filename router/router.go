9484955dae1906d1784330ba891ad25fca2c1fef
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2023-09-02 00:26:02 -0400

    feat: add router logic

--- router/router.go
+++ router/router.go
@@ -4,10 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func Initialize() {
 	router := gin.Default()
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
 	_ = router.Run(":8080")
 }
