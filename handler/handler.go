215517d5255d8a63b24e483d99a8ba1ffbd9b504
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2023-09-02 18:38:32 -0400

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/Robertotheto/golang-api/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	db = config.GetSQLite()
+	logger = config.GetLogger("handler")
+}
