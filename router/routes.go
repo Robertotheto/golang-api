64bd35ea5efb6317bc1ee4c552c92f7e6e77cea3
Roberto dos Santos Filho <robertodossantosfilho1001@gmail.com>
2023-09-03 19:34:42 -0400

    feat: add swagger configuration

--- router/routes.go
+++ router/routes.go
@@ -1,13 +1,18 @@
 package router
 
 import (
+	docs "github.com/Robertotheto/golang-api/docs"
 	"github.com/Robertotheto/golang-api/handler"
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func initializeRoutes(router *gin.Engine) {
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 
@@ -19,4 +24,6 @@ func initializeRoutes(router *gin.Engine) {
 
 		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
+	// Initialize Swagger
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
